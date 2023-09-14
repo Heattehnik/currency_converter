cf2c4e7f57282994d0b43a12fcbdbb5bfb4b715e
roman <roman.ecm@mail.ru>
2023-09-14 16:44:03 +0500

    add users migrations

--- users/models.py
+++ users/models.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-NULLABLE = {'blank': True, 'null': True}
+NULLABLE = {"blank": True, "null": True}
 
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name='почта')
+    email = models.EmailField(unique=True, verbose_name='Почта')
     name = models.CharField(max_length=150, verbose_name='Имя')
-    surname = models.CharField(max_length='150', verbose_name='Фамилия')
-    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    surname = models.CharField(max_length=150, verbose_name='Фамилия')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.name} {self.surname}({self.email})"
