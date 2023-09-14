b58be8cf6d63bbb49f063bb30ea6f4a125fc2043
Heattehnik <gadjka@mail.ru>
2023-09-14 18:53:24 +0500

    change model field rate.

--- converter/models.py
+++ converter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CurrencyRate(models.Model):
     currency_code = models.CharField(max_length=3, unique=True)
-    rate = models.DecimalField(max_digits=10, decimal_places=9)
+    rate = models.DecimalField(max_digits=20, decimal_places=15)
 
     def __str__(self):
         return self.currency_code
